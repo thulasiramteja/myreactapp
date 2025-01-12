dee53d21752d3d1a04865dea0de31d410d5ccdc6
thulasiramteja <110587811+thulasiramteja@users.noreply.github.com>
2025-01-13 03:09:40 +0530

    Update UserForm.js

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -7,7 +7,7 @@ const UserForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post('http://myreactapp.railway.internal:5000/users/add', { name, email });
+    await axios.post('http://myreactapp-production-2d80.up.railway.app/:5000/users/add', { name, email });
     setName('');
     setEmail('');
   };
