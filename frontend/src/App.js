8fd94026610e279e9c9ab0975231410be8f3eaf0
thulasiramteja <110587811+thulasiramteja@users.noreply.github.com>
2025-01-13 03:09:58 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const { data } = await axios.get('http://myreactapp.railway.internal:5000/users/view');
+      const { data } = await axios.get('http://myreactapp-production-2d80.up.railway.app:5000/users/view');
       setUsers(data);
     };
     fetchUsers();
